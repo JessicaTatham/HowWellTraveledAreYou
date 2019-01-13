ea66265fe82a786f12c0dbbc397b16713de16cee
JessicaTatham <jectatham@gmail.com>
2019-01-13 11:57:30 -0800

    Functionality for calculating percentage of world visited working

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			country: '',
+			countries: []
+		};
+
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onChange = this.onChange.bind(this);
+		this.calculateTravel = this.calculateTravel.bind(this)
+;	}
+
+	onChange(event) {
+		this.setState({
+	      country: event.target.value
+	    }); 
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+		const duplicate = this.state.countries.indexOf(this.state.country);
+		if (duplicate > -1) {
+			alert('This country has already been added to your list.');
+		} else {
+			let updatedCountries = this.state.countries;
+			updatedCountries.push(this.state.country);
+			this.setState({countries: updatedCountries});
+		}
+	}
+
+	calculateTravel(event) {
+		event.preventDefault();
+	}
+
+	render() {
+
+		let percentOfWorld = '0';
+		if (this.state.countries.length > 0) {
+			percentOfWorld = this.state.countries.length / 195 * 100;
+			percentOfWorld = percentOfWorld.toFixed(2);
+		}
+
+		return (
+			<div>
+				<h1> How Well Traveled Are You? </h1>
+
+				<p> Enter in the countries you have visited</p>
+
+				<form onSubmit={this.onSubmit} >
+					<input type='text' placeholder='Country name' value={this.state.country} onChange={this.onChange} />
+					<input type='submit' value='Add Country'/>
+				</form>
+
+				<p>You have traveled to {percentOfWorld} % of the world</p>
+
+			</div>
+
+		)
+	}
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name]-[hash:8].[ext]'
+                },
+            },
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
